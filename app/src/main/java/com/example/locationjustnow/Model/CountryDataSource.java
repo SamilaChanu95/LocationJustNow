ec6e5d32351c58e9d11e8b517778a519b0626155
SamilaChanu95 <chanukasamila@gmail.com>
2019-09-14 22:14:36 +0530

    Add create class CountryDataSource in CountryDataSource java file.

--- app/src/main/java/com/example/locationjustnow/Model/CountryDataSource.java
+++ app/src/main/java/com/example/locationjustnow/Model/CountryDataSource.java
@@ -1,4 +1,19 @@
 package com.example.locationjustnow.Model;
 
+import java.util.Hashtable;
+
 public class CountryDataSource {
+
+    public static final String COUNTRY_KEY = "country";
+    public static final float MINIMUM_CONFIDENCE_LEVEL = 0.4F;
+    public static final String DEFAULT_COUNTRY_NAME = "Canada";
+    public static final double DEFAULT_COUNTRY_LATITUDE = 59.969151;
+    public static final double DEFAULT_COUNTRY_LONGITUDE = -111.459050;
+    public static final String DEFAULT_MESSAGE = "Welcome!";
+
+    private Hashtable<String, String> countriesAndMessages;
+
+
+
+
 }
